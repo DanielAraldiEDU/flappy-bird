bde40d1a3e944d2306542fd6fd2266d0ad4d8f9e
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-05-02 22:04:04 -0300

    fix: removed bird and obstacles trails. add tractive to colision and score

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
-/*Versão beta desenvolvida por Thiago Felski Pereira para disciplina de Algoritmos*/
+/**
+ * Autores:
+ *
+ *  Bruno Ferreira Jimenez
+ *  Daniel Sansão Araldi
+ *  Enzo Luigi Schork
+ *  Jonathas Meine
+ */
+
+/* Versão beta desenvolvida por Thiago Felski Pereira para disciplina de Algoritmos */
 #include <iostream>
 #include <windows.h>
 #include <conio.h> ///para o getch()
+#include <time.h>
 /// https://stackoverflow.com/questions/34474627/linux-equivalent-for-conio-h-getch
 
 using namespace std;
 
 /**
-    F L A P B I R D (M2 Algoritmos)
+    F L A P P Y B I R D (M2 Algoritmos)
     - Lembrando: não permitido o uso de funções e variáveis compostas como structs, vetores e matrizes
     - Corrigir os rastros (pássaro e obstáculos)
     - Implementar colisão
@@ -32,17 +42,33 @@ int main()
   // FIM: COMANDOS PARA REPOSICIONAR O CURSOR NO INÍCIO DA TELA
   /// ALERTA: NÃO MODIFICAR O TRECHO DE CÓDIGO, ACIMA.
 
-  int bird_x = 5, bird_y = 10;
+  // LIMPA O TERMINAL AO COMEÇAR O JOGO
+  system("cls");
+
+  srand(time(NULL));
+
+  // DECLAÇÃO DE VARIÁVEIS
+  int bird_x = 100, bird_y = 10;
+  int placar_x = 1, placar_y = 1;
   int obstaculo_x = 129, obstaculo_y;
-  int tecla;
+  int tecla, numeroAleatorio, placar = 0;
 
   /// DESENHO DO CENÁRIO
   cout << "----------------------------------------------------------------------------------------------------------------------------------";
   cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
   cout << "----------------------------------------------------------------------------------------------------------------------------------";
 
+  // GERA NÚMEROS ALEATÓRIOS 3 e 17
+  numeroAleatorio = rand() % 15 + 3;
+
   while (true)
   { // esse laço faz o jogo rodar para sempre
+
+    // POSICIONA O PLACAR
+    coord.X = placar_x;
+    coord.Y = placar_y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    cout << "PLACAR: " << placar;
 
     /// POSICIONA O PÁSSARO
     coord.X = bird_x;
@@ -57,7 +83,9 @@ int main()
       coord.X = obstaculo_x;
       coord.Y = obstaculo_y;
       SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-      if (obstaculo_y < 5 || obstaculo_y > 7)
+
+      // CRIA ESPAÇAMENTO DE DOIS ESPAÇOS PARA CIMA E PARA BAIXO NO OBSTÁCULO
+      if (obstaculo_y < numeroAleatorio - 2 || obstaculo_y > numeroAleatorio + 2)
       {
         cout << char(219);
       }
@@ -67,6 +95,10 @@ int main()
       }
 
       obstaculo_y++;
+
+      coord.X = obstaculo_x + 4;
+      SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+      cout << " ";
     }
 
     /// VERIFICA COLISÃO
@@ -77,7 +109,19 @@ int main()
       tecla = getch(); // verifica comando do jogador
     }
 
-    /// PÁSSARO CAI 1 POSIÇÃO SE NÃO FOI PRESSIONADO PARA SUBIR
+    // PAUSA O JOGO CASO HAJA UMA COLISÃO COM O OBSTÁCULO
+    if (obstaculo_x == bird_x && (bird_y < numeroAleatorio - 2 || bird_y > numeroAleatorio + 2))
+    {
+      system("pause");
+    }
+
+    // ADICIONA PONTUAÇÃO AO PLACAR AO PASSAR DO OBSTÁCULO
+    if (bird_x == obstaculo_x + 4)
+    {
+      placar++;
+    }
+
+    // PÁSSARO CAI 1 POSIÇÃO SE NÃO FOR PRESSIONADO PARA SUBIR
     if (tecla == 'w')
     {
       bird_y--;
@@ -88,11 +132,35 @@ int main()
       bird_y++;
     }
 
+    // PAUSA O JOGO CASO HAJA UMA COLISÃO COM O TETO OU O CHÃO
+    if (bird_y <= -1 || bird_y >= 21)
+    {
+      system("pause");
+    }
+
     /// OBSTÁCULO AVANÇA UMA POSIÇÃO PARA ESQUERDA
     obstaculo_x--;
 
     /// TEMPO DE ESPERA
-    Sleep(500);
+    Sleep(150);
+
+    // REMOVE O RASTRO DO PÁSSARO PARA CIMA
+    coord.Y = bird_y - 1;
+    coord.X = bird_x;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    if (!(bird_y == 1))
+    {
+      cout << " ";
+    }
+
+    // REMOVE O RASTRO DO PÁSSARO PARA BAIXO
+    coord.Y = bird_y + 1;
+    coord.X = bird_x;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    if (!(bird_y == 19))
+    {
+      cout << " ";
+    }
   }
 
   return 0;
